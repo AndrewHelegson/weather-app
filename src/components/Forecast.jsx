5bc1d7df76a987feb02e5e23a6547f9baece045b
AndrewHelegson <a.khrustikov@gmail.com>
2022-11-05 21:58:19 +0400

    first deploy

---
+++ src/components/Forecast.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { iconUrlFromCode } from './Services/weatherServices'
+
+const Forecast = ({ title, items }) => {
+    return (
+        <div>
+            <div className='
+      flex
+      items-center
+      justify-center
+      mt-6
+      '>
+                <p className='
+        text-white
+        font-medium
+        uppercase
+        '>{title}</p>
+            </div>
+            <hr className='my-2' />
+            <div className='
+            flex
+            flex-row
+            items-center
+            justify-between
+            text-white
+            ' >
+                {items.map(item => (
+
+                    <div key={item.temp} className='
+                    flex
+                    flex-col
+                    items-center
+                    ' >
+                        <p className='
+                        font-light
+                        text-sm
+                        '>
+                            {item.title}
+                        </p>
+                        <img src={iconUrlFromCode(item.icon)}
+                            alt="weather"
+                            className='w-12 my-1'
+                        />
+                        <p className='font-medium' >{`${item.temp.toFixed()}°`}</p>
+                    </div>
+
+                ))}
+
+
+            </div>
+        </div>
+    )
+}
+
+export default Forecast
